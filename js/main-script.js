825d10f682af4b81a5d26ba180b9deaa87b458ac
Gustavo <gustavo.augusto2001@hotmail.com.br>
2022-11-02 15:53:36 -0300

    finalizado 🎉🎉

--- js/main-script.js
+++ js/main-script.js
@@ -10,6 +10,7 @@ var aspectRatio = document.querySelectorAll('.side-control-page-2 .aspect li')
 var controlCropper = document.querySelectorAll('.bottom-control .ctrl-cropper svg')
 var lockCropper = document.querySelectorAll('.bottom-control .lock svg')
 var dargMode = document.querySelectorAll('.bottom-control .drag-mode svg')
+var spanresposta = document.getElementById("resultadoApiImagem")
 
 
 
@@ -19,6 +20,7 @@ hiddenUpload.onchange = () => {
     // apdate on new file selected issue removed here
     document.querySelector('.image-workspace').innerHTML = `<img src="" alt="">`
     var image_workspace = document.querySelector('.image-workspace img')
+    spanresposta.innerHTML = ""
 
     var file = hiddenUpload.files[0]
     var url = window.URL.createObjectURL(new Blob([file], { type: 'image/jpg' }))
@@ -51,22 +53,22 @@ hiddenUpload.onchange = () => {
             // Enviar imagem para o tensorflow
             actionButton[1].onclick = () => {
                 actionButton[1].innerText = '...'
+                spanresposta.innerHTML = ""
+                document.getElementsByClassName("loader")[0].style.display = "block"
                 cropper.getCroppedCanvas({ width: 100, heigth: 200 }).toBlob((blob) => {
                     var downloadUrl = window.URL.createObjectURL(blob)
+                    console.log("blob => ")
+                    console.log(blob)
                     var a = document.createElement('a')
                     a.href = downloadUrl
                     console.log(downloadUrl)
                     a.download = 'cropped-image.jpg' // output image name
                     var data = {
-                        "url": downloadUrl.split("blob:")[1]
+                        "url": downloadUrl
                     }
 
-
-                    var CNN = new Promessa(data, undefined);
-                    CNN.then(
-                        function (dados) {
-                            console.log(dados)
-                        })
+                    var arquivo = new File([blob], "pupa.png", { type: blob.type })
+                    uploadImagemEctare(arquivo)
                     // a.click()
                     // actionButton[1].innerText = 'Download'
                 })
@@ -86,9 +88,7 @@ function Promessa(parametros, load) { // Faz a comunicação com o servidor
             body: JSON.stringify(parametros),
             // body: parametros,
             headers: {
-                "Accept": "application/json, text/javascript, */*; q=0.01",
-                "Access-Control-Allow-Headers": "Content-Type",
-
+                "Content-Type": "application/json"
                 // 'Content-Type': 'application/json; charset=UTF-8'
                 // "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
             }
@@ -117,20 +117,54 @@ function Promessa(parametros, load) { // Faz a comunicação com o servidor
 
 
     });
+}
 
 
-    // return Promise.resolve(
-    // 	jQuery.ajax({
-    // 		type: "POST",
-    // 		dataType: "json",
-    // 		data: parametros,
-    // 		url: `${myApp["data"]["banco"]}/${server}?tela=${tela}`,
-    // 		headers:{
-    // 			'Authorization': `Bearer ${sessionStorage.token}`
-    // 		}
-    // 	})
-    // ).finally(function () {
-    // 		document.getElementById("load").style.display = "none";
+function uploadImagemEctare(url) {
 
-    // });
+
+    let req = new XMLHttpRequest();
+    let formData = new FormData();
+
+    req.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            var resposta = req.responseText;
+            console.log(resposta)
+
+            var data = {
+                "url": `C:/Ectare/${resposta}`
+            }
+
+
+            var CNN = new Promessa(data, undefined);
+            CNN.then(
+                function (dados) {
+                    actionButton[1].innerText = 'Exibir resultado'
+                    document.getElementsByClassName("loader")[0].style.display = "none"
+                    preencheResultado(dados.message)
+                })
+
+        }
+    });
+
+    formData.append('url', url);
+    // req.open('POST', "https://server.ectarepay.com.br/" + "_e" + "/ectare_adm?tela=SetUploadDocumentos&dbteste=false&idpessoa=" + idpessoa + "&tipo=" + nome, true);doradinho
+    // req.open("POST", "https://server.ectarepay.com.br/" + myApp.data.versao + "/ectare_adm?tela=InserePessoasExcelSextou&dbteste=false&user=" + myApp.data.usuario.pessoa.idpessoa + "&idsextou_solicitacao=" + document.getElementById('idsextou_solicitacao').value, true);
+    req.open("POST", "http://localhost:8080/EctareServer" + "/ectare_adm?tela=insereimgpupa&dbteste=false", true);
+    // req.open('POST', "https://server.ectarepay.com.br/" + "doradinho_e" + "/ectare_adm?tela=SetUploadDocumentos&dbteste=false&idpessoa=" + idpessoa +
+    // "&idtipodocumento=" + idtipodocumento, true);
+    // req.setRequestHeader('Authorization', `Bearer ${sessionStorage.token}`, )
+    req.send(formData);
+}
+
+function preencheResultado(resultado) {
+
+
+    if (resultado == "waste_pupae") {
+        spanresposta.innerHTML = "Waste Pupae"
+    } else if (resultado == 'parasitized_pupae') {
+        spanresposta.innerHTML = "Parasitized Pupae"
+    } else {
+        spanresposta.innerHTML = "Erro ao classificar imagem tente novamente"
+    }
 }
